8a807a68c1262dfc05f486883c487925437797d4
Woonsama <36832643+Woonsama@users.noreply.github.com>
2020-07-25 01:53:36 +0900

    CanvasBase, ObjectBase.cs 추가, GetComponentAttribute 추가

---
+++ New Unity Project/Assets/02.Scripts/KWS/Common/CanvasBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasBase : MonoBehaviour
+{
+    protected virtual IEnumerator OnShowCoroutine()
+    {
+        yield break;
+    }
+
+    protected virtual IEnumerator OnEnableCoroutine()
+    {
+        yield break;
+    }
+
+    protected virtual IEnumerator OnDisableCoroutine()
+    {
+        yield break;
+    }
+
+    private void Awake()
+    {
+        StartCoroutine(OnShowCoroutine());
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(OnEnableCoroutine());
+    }
+
+    private void OnDisable()
+    {
+        StartCoroutine(OnDisableCoroutine());
+    }
+}
---
+++ New Unity Project/Assets/02.Scripts/KWS/Common/ObjectBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBase : MonoBehaviour
+{
+    protected virtual IEnumerator OnStartCoroutine()
+    {
+        yield break;
+    }
+
+    private void Awake()
+    {
+        StartCoroutine(OnStartCoroutine());
+    }
+}
