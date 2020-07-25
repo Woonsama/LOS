9c69edbef37b615049ae9f88c10dbf23e6ee1eae
Woonsama <36832643+Woonsama@users.noreply.github.com>
2020-07-25 22:01:27 +0900

    타일 추가, 팔레트 추가, 리소스 추가, 사운드 추가,

--- New Unity Project/Assets/02.Scripts/KWS/Enemy.cs
+++ New Unity Project/Assets/02.Scripts/KWS/Enemy.cs
@@ -2,17 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : ObjectBase
 {
-    // Start is called before the first frame update
-    void Start()
+
+    Vector3 playerPos;
+
+    /// <summary>
+    /// 플레이어와 몬스터가 가까워 졌는지 체크
+    /// </summary>
+    protected bool isNear;
+    /// <summary>
+    /// 몬스터가 죽었는지 체크
+    /// </summary>
+    private bool isDead;
+
+    /// <summary>
+    /// 얼마나 가까우면 반응하는지 거리
+    /// </summary>
+    [Header("몬스터가 반응하는 거리")]
+    public float distance_Near;
+
+    [Header("몬스터 체력")]
+    public float hp;
+
+    [Header("몬스터 공격력")]
+    public float attack;
+
+    protected override IEnumerator OnStartCoroutine()
     {
-        
+        isNear = false;
+        playerPos = GameObject.FindWithTag("Player").GetComponent<Transform>().position;
+
+        StartCoroutine(Near_Check_Coroutine());
+        yield break;        
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual IEnumerator State_Coroutine()
     {
-        
+        yield break;
+    }
+
+    private IEnumerator Near_Check_Coroutine()
+    {
+        while(!isNear)
+        {
+            if(Mathf.Abs(transform.position.x - playerPos.x) <= distance_Near ||
+                Mathf.Abs(transform.position.y - playerPos.y) <= distance_Near)
+            {
+                isNear = true;
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator  Dead_Check_Coroutine()
+    {
+        while(!isDead)
+        {
+            if (hp <= 0)
+            {
+                isDead = true;
+                Destroy(this.gameObject);
+                yield break;
+            }
+            yield return null;
+        }
     }
 }
